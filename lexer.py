5cef780c628e1b5013b7c848e02dfda5560235d8
Wilfred Hughes <me@wilfred.me.uk>
2011-11-11 01:27:51 +0000

    initial commit of crude lexer

---
+++ lexer.py
@@ -0,0 +1,50 @@
+import re
+
+class LexingError(Exception): pass
+
+# tokens:
+class Token(object):
+    def __init__(self, string):
+        self.string = string
+
+class Whitespace(Token):
+    pattern = r"\s+"
+
+class Variable(Token):
+    pattern = r"[a-zA-Z_][a-zA-Z_0-9]*"
+
+class Number(Token):
+    # yep, no support for floats or literals for other bases
+    pattern = r"[0-9]+"
+
+class String(Token):
+    pattern = r"\".*?\""
+
+class OpenParen(Token):
+    pattern = r"\("
+
+class CloseParen(Token):
+    pattern = r"\)"
+
+class BuiltIn(Token):
+    # we define a separate token to stop users overwriting these
+    # todo: check the Python language spec
+    pattern = r"\+|\*|-|/"
+
+def lex(string):
+    tokens = []
+
+    while string:
+        for token_class in Token.__subclasses__():
+
+            match = re.match(token_class.pattern, string)
+            if match:
+                tokens.append((token_class, match.group(0)))
+                string = string[match.end():]
+                break
+        else:
+            # if we reach this point, we haven't found any token that
+            # matches this string
+            raise LexingError('Could not lex remaining: "%s"' % string)
+
+    return tokens
